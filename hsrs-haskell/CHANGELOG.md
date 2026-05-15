1b5752411b04b56be925fe156d51022536556f15
Marko Vejnovic <marko@harmont.dev>
2026-05-15 14:11:16 -0700

    feat: add hsrs Haskell runtime package for Hackage
    Provides Hsrs.Runtime module with BorshBufferRaw, fromBorshBuffer,
    withBorshArg, and re-exports from Codec.Borsh. Users of generated
    bindings only need build-depends: hsrs.

---
+++ hsrs-haskell/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.1.0.0
+
+- Initial release
+- `Hsrs.Runtime` module with `fromBorshBuffer`, `withBorshArg`, and re-exports from `Codec.Borsh`
---
+++ hsrs-haskell/src/Hsrs/Runtime.hs
@@ -0,0 +1,42 @@
+module Hsrs.Runtime
+  ( -- * Borsh buffer FFI
+    BorshBufferRaw
+  , fromBorshBuffer
+    -- * Borsh argument marshalling
+  , withBorshArg
+    -- * Re-exports from Codec.Borsh
+  , BorshSize
+  , ToBorsh
+  , FromBorsh
+  , AsStruct
+  , serialiseBorsh
+  , deserialiseBorsh
+  ) where
+
+import Codec.Borsh (AsStruct, BorshSize, FromBorsh, ToBorsh, deserialiseBorsh, serialiseBorsh)
+import Data.ByteString (useAsCStringLen)
+import Data.ByteString.Unsafe (unsafePackCStringLen)
+import Data.Word (Word8, Word64)
+import Foreign (FinalizerPtr, Ptr, castPtr, newForeignPtr, withForeignPtr)
+
+data BorshBufferRaw
+
+foreign import ccall "hsrs_borsh_len"  c_hsrsBorshLen  :: Ptr BorshBufferRaw -> IO Word64
+foreign import ccall "hsrs_borsh_ptr"  c_hsrsBorshPtr  :: Ptr BorshBufferRaw -> IO (Ptr Word8)
+foreign import ccall "&hsrs_borsh_free" c_hsrsBorshFree :: FinalizerPtr BorshBufferRaw
+
+fromBorshBuffer :: FromBorsh a => Ptr BorshBufferRaw -> IO a
+fromBorshBuffer bufPtr = do
+  fp <- newForeignPtr c_hsrsBorshFree bufPtr
+  withForeignPtr fp $ \p -> do
+    len <- c_hsrsBorshLen p
+    dataPtr <- c_hsrsBorshPtr p
+    bs <- unsafePackCStringLen (castPtr dataPtr, fromIntegral len)
+    case deserialiseBorsh bs of
+      Left err -> error (show err)
+      Right val -> pure val
+
+withBorshArg :: ToBorsh a => a -> (Ptr Word8 -> Word64 -> IO b) -> IO b
+withBorshArg val f =
+  useAsCStringLen (serialiseBorsh val) $ \(ptr, len) ->
+    f (castPtr ptr) (fromIntegral len)
